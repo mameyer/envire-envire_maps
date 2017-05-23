6e0cbaedbfcd9108e01f94f55e567ee33402e38a
Manuel Meyer <manuel.meyer@dfki.de>
2017-05-23 13:29:30 +0200

    update plugin for mls maps of SpatioTemporal type.

--- viz/SpatioTemporalMLSMap.cpp
+++ viz/SpatioTemporalMLSMap.cpp
@@ -1,71 +1,30 @@
-//
-// Copyright (c) 2015, Deutsches Forschungszentrum für Künstliche Intelligenz GmbH.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-// * Redistributions of source code must retain the above copyright notice, this
-//   list of conditions and the following disclaimer.
-//
-// * Redistributions in binary form must reproduce the above copyright notice,
-//   this list of conditions and the following disclaimer in the documentation
-//   and/or other materials provided with the distribution.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-
-#include <iostream>
-#include "SpatioTemporalMLSMapKalman.hpp"
+#include "SpatioTemporalMLSMap.hpp"
+#include <envire_core/items/SpatioTemporal.hpp>
 
 using namespace vizkit3d;
 
-struct SpatioTemporalMLSMapKalman::Data {
-    // Copy of the value given to updateDataIntern.
-    //
-    // Making a copy is required because of how OSG works
-    envire::core::SpatioTemporal<maps::grid::MLSMapKalman> data;
-};
-
-
-SpatioTemporalMLSMapKalman::SpatioTemporalMLSMapKalman()
-    : p(new Data)
+SpatioTemporalMLSMap::SpatioTemporalMLSMap()
 {
 }
 
-SpatioTemporalMLSMapKalman::~SpatioTemporalMLSMapKalman()
+SpatioTemporalMLSMap::~SpatioTemporalMLSMap()
 {
-    delete p;
 }
 
-osg::ref_ptr<osg::Node> SpatioTemporalMLSMapKalman::createMainNode()
+void SpatioTemporalMLSMap::updateData(const envire::core::SpatioTemporal<maps::grid::MLSMapKalman>& data)
 {
-    // Geode is a common node used for vizkit3d plugins. It allows to display
-    // "arbitrary" geometries
-    return new osg::Geode();
+    MLSMapVisualization::updateData(data.data);
 }
 
-void SpatioTemporalMLSMapKalman::updateMainNode ( osg::Node* node )
+void SpatioTemporalMLSMap::updateData(const envire::core::SpatioTemporal< maps::grid::MLSMapPrecalculated >& data)
 {
-    osg::Geode* geode = static_cast<osg::Geode*>(node);
-    // Update the main node using the data in p->data
+    MLSMapVisualization::updateData(data.data);
 }
 
-void SpatioTemporalMLSMapKalman::updateDataIntern(envire::core::SpatioTemporal<maps::grid::MLSMapKalman> const& value)
+void SpatioTemporalMLSMap::updateData(const envire::core::SpatioTemporal< maps::grid::MLSMapSloped >& data)
 {
-    p->data = value;
-    std::cout << "got new sample data" << std::endl;
+    MLSMapVisualization::updateData(data.data);
 }
 
 //Macro that makes this plugin loadable in ruby, this is optional.
-VizkitQtPlugin(SpatioTemporalMLSMapKalman)
-
+VizkitQtPlugin(SpatioTemporalMLSMap)
