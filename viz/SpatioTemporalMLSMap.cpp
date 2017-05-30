3ecb65923044bec506933cb6e3d635f682019caa
Manuel Meyer <manuel.meyer@dfki.de>
2017-05-30 17:13:58 +0200

    set the visualization frame in the plugins update method.

--- viz/SpatioTemporalMLSMap.cpp
+++ viz/SpatioTemporalMLSMap.cpp
@@ -11,20 +11,5 @@ SpatioTemporalMLSMap::~SpatioTemporalMLSMap()
 {
 }
 
-void SpatioTemporalMLSMap::updateData(const envire::core::SpatioTemporal<maps::grid::MLSMapKalman>& data)
-{
-    MLSMapVisualization::updateData(data.data);
-}
-
-void SpatioTemporalMLSMap::updateData(const envire::core::SpatioTemporal< maps::grid::MLSMapPrecalculated >& data)
-{
-    MLSMapVisualization::updateData(data.data);
-}
-
-void SpatioTemporalMLSMap::updateData(const envire::core::SpatioTemporal< maps::grid::MLSMapSloped >& data)
-{
-    MLSMapVisualization::updateData(data.data);
-}
-
 //Macro that makes this plugin loadable in ruby, this is optional.
 VizkitQtPlugin(SpatioTemporalMLSMap)
